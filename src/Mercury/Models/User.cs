ed53947d923849590cb523379ec84310aa53f3f7
maxaytt <Maxaytt@gmail.com>
2024-02-05 16:20:43 +0100

    feat(models): add User model

---
+++ src/Mercury/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Mercury.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Firstname { get; set; } = null!;
+    public string Lastname { get; set; } = null!;
+}
