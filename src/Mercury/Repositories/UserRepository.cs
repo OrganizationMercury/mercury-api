b97ad0cbd24d8c6474727955928ee2f3ffbab943
maxaytt <Maxaytt@gmail.com>
2024-02-05 16:39:46 +0100

    feat(repositories): add user repository

---
+++ src/Mercury/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Mercury.Models;
+using Neo4jClient;
+
+namespace Mercury.Repositories;
+
+public class UserRepository
+{
+    private readonly IGraphClient _client;
+
+    public UserRepository(IGraphClient client)
+    {
+        _client = client;
+    }
+
+    public async Task CreateUserAsync(User user)
+    {
+        await _client.Cypher
+            .Create("""
+                    (user:User { 
+                    id: id,
+                    firstname: firstname,
+                    lastname: lastname
+                    })
+                    """)
+            .WithParams(new
+            {
+                id = user.Id,
+                firstname = user.Firstname,
+                lastname = user.Lastname
+            })
+            .ExecuteWithoutResultsAsync();
+    }
+
+    public async Task<User> GetUserAsync(Guid id)
+    {
+        var users = await _client.Cypher
+            .Match("(user:User)")
+            .Where("user.id = $id")
+            .WithParam("id", new { id })
+            .Return(user => user.As<User>())
+            .Limit(1)
+            .ResultsAsync;
+
+        return users.Single();
+    }
+}
