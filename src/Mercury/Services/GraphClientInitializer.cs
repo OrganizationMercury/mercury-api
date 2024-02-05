b7b95fc90a46330f2b6142dd0b26b403d85fc431
maxaytt <Maxaytt@gmail.com>
2024-02-05 16:35:45 +0100

    feat: add graph client initializer

---
+++ src/Mercury/Services/GraphClientInitializer.cs
@@ -0,0 +1,24 @@
+﻿using Neo4jClient;
+
+namespace Mercury.Services;
+
+public class GraphClientInitializer : IHostedService
+{
+    private readonly IGraphClient _client;
+
+    public GraphClientInitializer(IGraphClient client)
+    {
+        _client = client;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await _client.ConnectAsync();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _client.Dispose();
+        return Task.CompletedTask;
+    }
+}
