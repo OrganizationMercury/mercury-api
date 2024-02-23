41cde52880bb9788a2a4f32009ddab69a6ab5866
Maxaytt <maxaytt@gmail.com>
2024-02-23 17:58:35 +0100

    feat(repositories): add update and delete methods

--- src/Mercury/Repositories/UserRepository.cs
+++ src/Mercury/Repositories/UserRepository.cs
@@ -17,13 +17,13 @@ public class UserRepository
         {
             var data = await runner.RunAsync(
                 """
-                MATCH (user: User { Id: $id })
+                MATCH (user: User { Id: $Id })
                 RETURN {
                     Id: user.Id,
                     Firstname: user.Firstname,
                     Lastname: user.Lastname
                 }
-                """, new { id = id.ToString() });
+                """, new { Id = id.ToString() });
             var record = await data.SingleAsync();
             return record[0].Adapt<User>();
         });
@@ -36,15 +36,44 @@ public class UserRepository
             await runner.RunAsync(
                 """
                 CREATE (user:User {
-                    Id: $id,
-                    Firstname: $firstname,
-                    Lastname: $lastname
+                    Id: $Id,
+                    Firstname: $Firstname,
+                    Lastname: $Lastname
                 })
                 """, new
                 {
-                    id = user.Id.ToString(),
-                    firstname = user.Firstname,
-                    lastname = user.Lastname
+                    Id = user.Id.ToString(),
+                    user.Firstname,
+                    user.Lastname
                 }));
+    }
+
+    public async Task UpdateUserAsync(User user)
+    {
+        await using var session = _driver.AsyncSession();
+        await session.ExecuteWriteAsync(async runner =>
+            await runner.RunAsync(
+                """
+                MATCH (user:User { Id: $Id })
+                SET user.Firstname = $Firstname,
+                    user.Lastname = $Lastname
+                """, new
+                {
+                    Id = user.Id.ToString(),
+                    user.Firstname,
+                    user.Lastname
+                }));
+    }
+    
+    public async Task DeleteUserAsync(Guid id)
+    {
+        await using var session = _driver.AsyncSession();
+        await session.ExecuteWriteAsync(async runner =>
+            await runner.RunAsync(
+            """
+            MATCH (user:User { Id: $Id })
+            DETACH DELETE user
+            """, new { Id = id.ToString() }
+            ));
     }
 }
