ff3ade47869181add445ab484d75a499d663509d
Maxaytt <maxaytt@gmail.com>
2024-03-22 12:03:16 +0100

    feat(persistence): add db context

---
+++ src/Infrastructure.Persistence/AppDbContext.cs
@@ -0,0 +1,9 @@
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public required DbSet<User> Users { get; set; }
+}
