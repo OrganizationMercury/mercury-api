a047a03ce7209871f40a5a02fb9b4e66b4d83fdd
Maxaytt <maxaytt@gmail.com>
2024-03-22 13:08:58 +0100

    feat(controllers): get user from context

--- src/Api/Controllers/UsersController.cs
+++ src/Api/Controllers/UsersController.cs
@@ -1,18 +1,25 @@
 ﻿using Api.Dto;
 using Domain.Models;
+using Infrastructure.Persistence;
 using Infrastructure.Persistence.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class UsersController(UserRepository users) : ControllerBase
+public class UsersController(UserRepository users, AppDbContext context) : ControllerBase
 {
     [HttpGet]
-    public async Task<IActionResult> GetUserAsync([FromQuery] Guid id)
+    public async Task<IActionResult> GetUserAsync([FromQuery] Guid id, 
+        CancellationToken cancellationToken)
     {
-        var user = await users.GetUserAsync(id);
+        var user = await context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(user => user.Id == id, cancellationToken);
+        
+        if (user is null) throw new Exception(id + " user was not found!");
         return Ok(user);
     }
 
