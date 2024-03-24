352736d19bb965d735b52db80b95e3ea8150aeb5
Maxaytt <maxaytt@gmail.com>
2024-03-24 19:30:43 +0100

    feat(controllers): add http annotations

--- src/Api/Controllers/InterestsController.cs
+++ src/Api/Controllers/InterestsController.cs
@@ -7,13 +7,15 @@ namespace Api.Controllers;
 [Route("[controller]")]
 public class InterestsController(InterestRepository interestsGraph) : ControllerBase
 {
+    [HttpGet]
     public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
     {
         var interests = await interestsGraph.GetAllAsync();
         return Ok(interests);
     }
 
-    public async Task<IActionResult> CreateIfNotExists([FromBody]string name)
+    [HttpPost]
+    public async Task<IActionResult> CreateIfNotExists([FromBody] string name)
     {
         await interestsGraph.CreateIfNotExistsAsync(name);
         return NoContent();
