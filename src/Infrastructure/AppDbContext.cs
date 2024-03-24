c372f6e2a7dba3aab5a241c82517bb3047fc5cfe
Maxaytt <maxaytt@gmail.com>
2024-03-24 19:02:21 +0100

    feat(persistence): add username index to users

--- src/Infrastructure/AppDbContext.cs
+++ src/Infrastructure/AppDbContext.cs
@@ -6,4 +6,11 @@ namespace Infrastructure;
 public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public required DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<User>()
+            .HasIndex(u => u.Username)
+            .IsUnique();
+    }
 }
