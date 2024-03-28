edeb637eadcf9edf74daff381fa7e2522add8319
Maxaytt <maxaytt@gmail.com>
2024-03-28 14:22:01 +0100

    feat(dto): add CreateUserDto

--- src/Api/Dto/UserDto.cs
+++ src/Api/Dto/UserDto.cs
@@ -1,3 +1,12 @@
+using Domain.Models;
+
 namespace Api.Dto;
 
-public record UserDto(string Firstname, string Lastname, string Username);
+public record CreateUserDto(string Firstname, string Lastname, string Username);
+
+public record UpdateUserDto(
+    Guid Id,
+    string Firstname,
+    string Lastname,
+    string Username,
+    byte[] ImageBytes);
