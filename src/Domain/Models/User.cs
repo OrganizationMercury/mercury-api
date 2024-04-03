e95442c77acbcb1b46a1e0075f4956d2d25096e7
Maxaytt <maxaytt@gmail.com>
2024-04-03 16:18:27 +0200

    refactor: rename Image to file

--- src/Domain/Models/User.cs
+++ src/Domain/Models/User.cs
@@ -4,7 +4,7 @@ public class User
 {
     public Guid Id { get; set; }
     public Guid? AvatarId { get; set; }
-    public Image? Avatar { get; set; } 
+    public File? Avatar { get; set; } 
     public string Firstname { get; set; } = null!;
     public string Lastname { get; set; } = null!;
     public string Username { get; set; } = null!;
