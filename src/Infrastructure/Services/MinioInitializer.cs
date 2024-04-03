e5ee8e54eeea6d65c1f12088243b10fcf55feb30
Maxaytt <maxaytt@gmail.com>
2024-04-03 16:16:23 +0200

    feat(persistence): add MinioInitializer

---
+++ src/Infrastructure/Services/MinioInitializer.cs
@@ -0,0 +1,23 @@
+using Domain;
+using Microsoft.Extensions.Hosting;
+using Minio;
+using Minio.DataModel.Args;
+
+
+namespace Infrastructure.Services;
+
+public class MinioInitializer(MinioClient client) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        var avatarExists = new BucketExistsArgs().WithBucket(BucketConstants.Avatar);
+        var exists = await client.BucketExistsAsync(avatarExists, cancellationToken);
+        if (!exists)
+        {
+            var avatarMake = new MakeBucketArgs().WithBucket(BucketConstants.Avatar);
+            await client.MakeBucketAsync(avatarMake, cancellationToken);
+        }
+    }
+
+    public Task StopAsync(CancellationToken _) => Task.CompletedTask;
+}
