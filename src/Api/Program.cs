a90e4a5a7b14cf41d59b6852f1b6c7b86d7cd989
Maxaytt <maxaytt@gmail.com>
2024-04-03 16:17:04 +0200

    refactor: move brackets

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -5,11 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options => options.AddPolicy(name: "AllowFrontend",
-        policyBuilder => policyBuilder
+builder.Services.AddCors(options => options
+        .AddPolicy(name: "AllowFrontend", policyBuilder => policyBuilder
                 .WithOrigins("http://localhost:4200")
                 .AllowAnyMethod()
-                .AllowAnyHeader()));
+                .AllowAnyHeader()
+        ));
 
 builder.Services.AddPersistenceServices(builder.Configuration);
 var app = builder.Build();
