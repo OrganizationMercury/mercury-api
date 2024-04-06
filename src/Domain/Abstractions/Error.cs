f843fb0e1088a9c8650f8e3d490bb3aab9947669
Maxaytt <maxaytt@gmail.com>
2024-04-06 13:32:48 +0200

    feat(domain): use Messages not found

--- src/Domain/Abstractions/Error.cs
+++ src/Domain/Abstractions/Error.cs
@@ -5,8 +5,7 @@ public record Error(string Message)
     public static NotFoundError NotFound(string type, string id) => new(type, id);
 }
 
-public sealed record NoneError() : Error(string.Empty);
 public sealed record NotFoundError : Error
 {
-    public NotFoundError(string type, string id) : base($"{type}: {id} was not found"){}
+    public NotFoundError(string type, string id) : base(Messages.NotFound(type, id)){}
 }
