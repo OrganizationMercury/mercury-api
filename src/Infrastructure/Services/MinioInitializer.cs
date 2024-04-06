27baa2ad2de372a55bfd898a250f7abf9eda619b
Maxaytt <maxaytt@gmail.com>
2024-04-06 12:37:05 +0200

    fix(persistence): add scope factory

--- src/Infrastructure/Services/MinioInitializer.cs
+++ src/Infrastructure/Services/MinioInitializer.cs
@@ -1,4 +1,5 @@
 using Domain;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Minio;
 using Minio.DataModel.Args;
@@ -6,10 +7,13 @@ using Minio.DataModel.Args;
 
 namespace Infrastructure.Services;
 
-public class MinioInitializer(IMinioClient client) : IHostedService
+public class MinioInitializer(IServiceScopeFactory scopeFactory) : IHostedService
 {
     public async Task StartAsync(CancellationToken cancellationToken)
     {
+        using var scope = scopeFactory.CreateScope();
+        var client = scope.ServiceProvider.GetRequiredService<IMinioClient>();
+        
         var avatarExists = new BucketExistsArgs().WithBucket(BucketConstants.Avatar);
         var exists = await client.BucketExistsAsync(avatarExists, cancellationToken);
         if (!exists)
