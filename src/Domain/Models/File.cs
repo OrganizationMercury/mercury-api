d35bbdce3ff1a5b098a6b1e04550c9d8a5d83d60
Maxaytt <maxaytt@gmail.com>
2024-04-06 12:35:55 +0200

    feat(model): add filed to File

--- src/Domain/Models/File.cs
+++ src/Domain/Models/File.cs
@@ -7,4 +7,5 @@ public class File
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
     public string Bucket { get; set; } = null!;
+    public string Extension { get; set; } = null!;
 }
