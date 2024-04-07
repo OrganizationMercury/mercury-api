e5f4632e6cb669402bf838256e682363f622b109
Maxaytt <maxaytt@gmail.com>
2024-04-07 16:40:13 +0200

    feat(models): add key annotation to Filename

--- src/Domain/Models/File.cs
+++ src/Domain/Models/File.cs
@@ -1,9 +1,10 @@
-
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Models;
 
 public class File
 {
+    [Key]
     public string Filename { get; set; } = null!;
     public Guid UserId { get; set; }
     public string Bucket { get; set; } = null!;
