ffe73fb6f2444f036847310a9d6a2398a544f6ee
Maxaytt <maxaytt@gmail.com>
2024-04-07 16:31:45 +0200

    feat(models): remove id, extension add Filename

--- src/Domain/Models/File.cs
+++ src/Domain/Models/File.cs
@@ -4,8 +4,7 @@ namespace Domain.Models;
 
 public class File
 {
-    public Guid Id { get; set; }
+    public string Filename { get; set; } = null!;
     public Guid UserId { get; set; }
     public string Bucket { get; set; } = null!;
-    public string Extension { get; set; } = null!;
 }
