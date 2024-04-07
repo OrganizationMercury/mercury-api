f35e6c7a39a6401dc5d8a3098c9533af28f3b90e
Maxaytt <maxaytt@gmail.com>
2024-04-07 16:34:31 +0200

    feat(domain): add NotFound message function

--- src/Domain/Abstractions/Messages.cs
+++ src/Domain/Abstractions/Messages.cs
@@ -3,4 +3,5 @@ namespace Domain.Abstractions;
 public static class Messages
 {
     public static string NotFound(string type, object id) => $"{type}: {id} was not found";
+    public static string NotFound<T>(object id) => $"{nameof(T)}: {id} was not found";
 }
