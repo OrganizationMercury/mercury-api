66a886c0619ef66897b605420a3bb79108385325
Maxaytt <maxaytt@gmail.com>
2024-05-24 14:11:18 +0200

    fix(persistence): add onModelCreating

--- src/Infrastructure/AppDbContext.cs
+++ src/Infrastructure/AppDbContext.cs
@@ -13,6 +13,8 @@ public sealed class AppDbContext(DbContextOptions<AppDbContext> options)
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        base.OnModelCreating(builder);
+        
         builder.Entity<User>()
             .HasIndex(u => u.UserName)
             .IsUnique();
