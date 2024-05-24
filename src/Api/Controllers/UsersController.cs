676d39fd8c0ff301e5ac623d3ef4bcfece3fd532
Maxaytt <maxaytt@gmail.com>
2024-05-24 11:14:15 +0200

    refactor(controllers): use user service in users controller

--- src/Api/Controllers/UsersController.cs
+++ src/Api/Controllers/UsersController.cs
@@ -1,30 +1,18 @@
 ﻿using Api.Dto;
-using Domain;
+using Api.Services;
 using Domain.Abstractions;
-using Domain.Models;
-using Infrastructure;
-using Infrastructure.Repositories;
-using Mapster;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using File = Domain.Models.File;
 
 namespace Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class UsersController(
-    UserRepository usersGraph,
-    AppDbContext context,
-    FileRepository fileRepository) : ControllerBase
+public class UsersController(UserService users) : ControllerBase
 {
     [HttpGet]
     public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
     {
-        var usersList = await context.Users
-            .AsNoTracking()
-            .ToListAsync(cancellationToken: cancellationToken);
-        
+        var usersList = await users.GetAllAsync(cancellationToken);
         return Ok(usersList);
     }
     
@@ -32,64 +20,17 @@ public class UsersController(
     public async Task<IActionResult> GetByIdAsync(Guid id, 
         CancellationToken cancellationToken)
     {
-        var user = await context.Users
-            .Include(user => user.Avatar)
-            .AsNoTracking()
-            .FirstOrDefaultAsync(user => user.Id == id, cancellationToken);
-        
+        var user = await users.GetByIdAsync(id, cancellationToken);
         if (user is null) return NotFound(Messages.NotFound(nameof(user), id));
+        
         return Ok(user);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> CreateAsync([FromBody] CreateUserDto request,
-        CancellationToken cancellationToken)
-    {
-        var user = request.Adapt<User>();
-        user.Id = Guid.NewGuid();
-
-        await context.Users.AddAsync(user, cancellationToken);
-        await context.SaveChangesAsync(cancellationToken);
-        await usersGraph.CreateAsync(user.Id);
-        return Ok(user.Id);
     }
 
     [HttpPut]
     public async Task<IActionResult> UpdateAsync([FromForm] UpdateUserDto request,
         CancellationToken cancellationToken)
     {
-        var user = await context.Users
-            .FirstOrDefaultAsync(user => user.Id == request.Id, cancellationToken);
-        if(user is null) return NotFound(Messages.NotFound<User>(request.Id));
-        
-        if (request.File is not null)
-        {
-            var avatar = await GetOrCreateAvatarAsync(user.Id, request.File.FileName, cancellationToken);
-            await fileRepository
-                .PutFileAsync(request.File, avatar, BucketConstants.Avatar, cancellationToken);
-            user.Avatar = avatar;
-        }
-        
-        request.Adapt(user);
-        await context.SaveChangesAsync(cancellationToken);
+        await users.UpdateAsync(request, cancellationToken);
         return NoContent();
-    }
-
-    private async Task<File> GetOrCreateAvatarAsync(Guid userId, string filename, CancellationToken cancellationToken)
-    {
-        var avatar = await context.Files.FirstOrDefaultAsync(file => file.UserId == userId, cancellationToken);
-        if (avatar is not null) return avatar;
-        
-        var fileId = Guid.NewGuid();
-        var fileExtension = Path.GetExtension(filename);
-        avatar = new File
-        {
-            Filename = $"{fileId}{fileExtension}",
-            UserId = userId,
-            Bucket = BucketConstants.Avatar,
-        };
-        await context.Files.AddAsync(avatar, cancellationToken);
-
-        return avatar;
     }
 }
