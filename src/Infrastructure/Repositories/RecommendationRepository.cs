2a6bce638a7d2e322e55572804dacf8894b0f5cc
Maxaytt <maxaytt@gmail.com>
2024-07-22 13:00:07 +0200

    feat(repositories): add recommendation repository

---
+++ src/Infrastructure/Repositories/RecommendationRepository.cs
@@ -0,0 +1,32 @@
+using Neo4j.Driver;
+
+namespace Infrastructure.Repositories;
+
+public class RecommendationRepository(IDriver driver)
+{
+    public async Task<Guid> RecommendAsync(Guid userId, int index)
+    {
+        await using var session = driver.AsyncSession();
+        return await session.ExecuteReadAsync(async runner =>
+        {
+            var data = await runner.RunAsync(
+                """
+                MATCH (currentUser:User { Id: $userId })
+                MATCH (currentUser)-[:LIKES]->(interests:Interest)
+                MATCH (otherUsers:User)-[:LIKES]->(interests)
+                WHERE otherUsers <> currentUser
+                WITH otherUsers, COUNT(interests) AS commonInterests
+                ORDER BY commonInterests DESC
+                RETURN otherUsers.Id AS userId
+                SKIP $index
+                LIMIT 1
+                """, new
+                {
+                    userId = userId.ToString(),
+                    index
+                });
+            return await data.SingleAsync(record => Guid.Parse(record["userId"].As<string>()));
+
+        });
+    }
+}
